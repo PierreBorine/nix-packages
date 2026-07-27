8d9cc8ce455dd58b20a0e7e9734d79bc96fd6b7c
PierreBorine <pierre.borine@protonmail.com>
2026-07-28 01:43:06 +0200

    hyprselect: 0.54.0-@1 -> 0.56.0-@1

--- pkgs/hyprselect/default.nix
+++ pkgs/hyprselect/default.nix
@@ -7,13 +7,13 @@
 }:
 hyprlandPlugins.mkHyprlandPlugin (finalAttrs: {
   pluginName = "hyprselect";
-  version = "0.54.0-@1";
+  version = "0.56.0-@1";
 
   src = fetchFromGitHub {
     owner = "jmanc3";
     repo = "hyprselect";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-IcF2TfXxxMmHgVAhHdH87jUAl5fWYbtmstUT0kkJ2Dc=";
+    hash = "sha256-ssvPEIXapSuRV+23hdBInj+HG2FwhQenMw+Bdh3cvzY=";
   };
 
   nativeBuildInputs = [versionCheckHook];
